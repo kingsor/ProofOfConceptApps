d3fda0910f62427b17f3aa0c27face1e838be836
kingsor <avventureweb@gmail.com>
2022-06-23 08:48:28 +0200

    Update nuget dependencies

---
+++ AccessDbFileCreation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOX;
+
+namespace ConsoleAppNetFramework
+{
+    class Program
+    {
+        static string ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=Database.mdb";
+
+        static void Main(string[] args)
+        {
+            CreateAccessDatabase();
+            
+            OpenAccessDatabase();
+
+            Console.WriteLine("Press Enter to finish.");
+            Console.ReadLine();
+        }
+
+        static void CreateAccessDatabase()
+        {
+            Catalog catalog = new Catalog();
+            catalog.Create(ConnectionString);
+            catalog = null;
+
+        }
+
+        static void OpenAccessDatabase()
+        {
+            OleDbConnection myConnection = new OleDbConnection(ConnectionString);
+            myConnection.Open();
+            OleDbCommand myCommand = new OleDbCommand();
+            myCommand.Connection = myConnection;
+            myCommand.CommandText = "CREATE TABLE table1([KEY] Text, [VALUE] Text)";
+            myCommand.ExecuteNonQuery();
+            myCommand.Connection.Close();
+        }
+    }
+}
--- MiscTestSample/Program.cs
+++ MiscTestSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MiscTestSample
 {
@@ -6,15 +7,33 @@ namespace MiscTestSample
     {
         static void Main(string[] args)
         {
-            TestExceptionFullStackTrace();
+            //TestExceptionFullStackTrace();
 
             //TestDateTimeFormatting();
 
-            var passPhrase = Environment.GetEnvironmentVariable("PASSPHRASE");
+            //var passPhrase = Environment.GetEnvironmentVariable("PASSPHRASE");
+
+            TestFizzBuzz();
 
             Console.ReadLine();
         }
 
+
+        static void TestFizzBuzz()
+        {
+            string FizzBuzz(int x) =>                // Local function, defined as an expression-bodied method
+                (x % 3 == 0, x % 5 == 0) switch
+                {    // Tuple definition  
+                    (true, true) => "FizzBuzz",      // Pattern-matching on the tuple values
+                    (true, _) => "Fizz",          // Discard (_) is used to omit
+                    (_, true) => "Buzz",          // the values we don't care about
+                    _ => x.ToString()
+                };
+
+            Enumerable.Range(1, 100)                 // Make a range of numbers from 1 to 100
+                .Select(FizzBuzz).ToList()           // Map each number to a corresponding FizzBuzz value
+                .ForEach(Console.WriteLine);         // Print the result to the console  
+        }
 
 
         static void TestExceptionFullStackTrace()
